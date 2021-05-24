d863108e71aed86d29eafdce12b658c4f32451d4
halki <s.halki0203@icloud.com>
2021-05-24 21:07:50 +0900

    view編集

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,17 @@ Route::get('/', 'ReviewController@index')->name('index');
 
 Auth::routes();
 
+Route::group(['middleware' => 'auth'], function(){
+
+    Route::get('/review', 'ReviewController@create')->name('create');
+    Route::post('/review/store', 'ReviewController@store')->name('store');
+
+});
+
+
+
+
 Route::get('/home', 'HomeController@index')->name('home');
+
 
 
